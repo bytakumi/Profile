7b419ec05aa75a4eb1a1d14df5d868fc5d93a8de
Takumi Suzuki <43519598+ibnr2hc@users.noreply.github.com>
2023-09-22 19:49:09 +0900

    自己紹介をレスポンシブ化した

--- components/selfIntroduction.tsx
+++ components/selfIntroduction.tsx
@@ -12,20 +12,18 @@ export default function SelfIntroduction() {
                 {/* OBJECT: About me (name, age, etc...) */}
                 <Card className="bg-transparent backdrop-blur-lg border-gray-200/20">
                     <CardHeader>
-                        <div className="flex justify-center items-center">
-                            <Image src="/photo.jpeg" width={90} height={90} className="rounded-md" alt="face photo" />
+                        <div className="flex flex-col md:flex-row justify-center items-center">
+                            <Image src="/photo.jpeg" width={90} height={90} className="rounded-md pb-4 md:pb-0" alt="face photo" />
                             <div className="flex flex-col ml-4 gap-1">
                                 {/* OBJECT: Photo */}
-                                <div className="font-bold text-3xl text-slate-900">
+                                <div className="font-bold text-xl md:text-3xl text-slate-900 text-center md:text-start">
                                     鈴木 拓海
-                                    <span className="ml-2 text-sm">
-                                        {/* TODO: 年齢を動的にする */}
+                                    <span className="ml-2 text-sm text-center md:text-start">
                                         ({age})
                                     </span>
                                 </div>
                                 {/* OBJECT: Career */}
                                 <div className="text-slate-700 text-xs">
-                                    {/* TODO: 経歴は動的にする */}
                                     システムエンジニア({career}) - 東京都
                                 </div>
                             </div>
@@ -34,10 +32,10 @@ export default function SelfIntroduction() {
                     </Card>
                 <CardContent>
                     {/* OBJECT: Self introduction */}
-                    <div className="pt-6 text-sm tracking-wide leading-relaxed text-slate-900">
-                        一個人が作るWebサービスでも全世界に影響を与えられる規模感に感動し、プライベートの時間でもWebサービスを作っています。<br />
-                        新しい技術に触れることも好きです。<br />
-                        しかし、Webサービスを作る際に新しい技術を使おうとするが故にサービスの開発速度が遅くなることが弱点です。
+                    <div className="pt-6 text-xs md:text-sm tracking-wide leading-relaxed text-slate-900 grid gap-2">
+                        <p>一個人が作るWebサービスでも全世界に影響を与えられる規模感に感動し、プライベートの時間でもWebサービスを作っています。</p>
+                        <p>新しい技術に触れることも好きです。</p>
+                        <p>しかし、Webサービスを作る際に新しい技術を使おうとするが故にサービスの開発速度が遅くなることが弱点です。</p>
                     </div>
                 </CardContent>
             </Card>
